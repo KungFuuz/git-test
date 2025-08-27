605ece74295b51faf47167c1bce32c6f4e87641a
KungFuuz <tounzx0006@hotmail.com>
2025-08-27 16:08:18 +0700

    Overide

--- TestGreeter.java
+++ TestGreeter.java
@@ -2,6 +2,7 @@ public class TestGreeter {
     public static void main (String []args){
         Greet g = new Greet();
         g.greet("john");
+        System.out.println(g.toString());
     }
     
 }
